fe6aed0032e0e1a94f6c948857059c94fb3e40bd
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-11-23 14:27:16 -0500

    Adds validations to user and saved_prescription models

--- app/models/saved_prescription.rb
+++ app/models/saved_prescription.rb
@@ -1,3 +1,11 @@
 class SavedPrescription < ApplicationRecord
   belongs_to :user
+
+  validates :drug_name, presence: true
+  validates :drug_dosage, presence: true
+  validates :manufacturer, presence: true
+  validates :ingredients, presence: true
+
+  # Serialize ingredients for easier storage and retrieval
+  serialize :ingredients, Array
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  before_save { email.downcase! }
+
+  has_many :saved_prescriptions, dependent: :destroy
 end
