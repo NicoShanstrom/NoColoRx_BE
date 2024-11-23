8230b01caa0f557425e0a6d54780ec22108c7d4f
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-11-23 14:41:09 -0500

    Sets up openfda service for initialization, connection, and get request

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,6 +70,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.before(:suite) do
+    WebMock.disable_net_connect!(allow_localhost: true)
+  end
 end
 
 require("./spec/support/factory_bot")
